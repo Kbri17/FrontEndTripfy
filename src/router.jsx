5aeb706bd6657c96699bffa55a7368f02efffd1f
Lorena Trujillo <lorenatrujillo991@gmail.com>
2025-02-19 22:42:07 -0500

    Agregando producto-detalle-galeria

--- src/router.jsx
+++ src/router.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import LoginForm from "./auth/LoginForm";
+import Details from "./pages/Details";
+import Producto from "./pages/Producto"
+import Galeria from "./pages/Galeria"
 
 const Root = () => (
   <div className="flex flex-col min-h-screen">
@@ -28,6 +31,18 @@ export const router = createBrowserRouter([
       {
         path: "/login",
         element: <LoginForm />,
+      },
+      {
+        path: "/producto",
+        element: <Producto/>,
+      },
+      {
+        path: "/details",
+        element: <Details />,
+      },
+      {
+        path: "/galeria",
+        element: <Galeria/>,
       },
     ],
   },
