180eb3452983df0ad472c062586c368ee3707a20
MoshMR <m.martinez.da@gmail.com>
2025-02-21 11:28:17 -0500

    Updated Carrusel component

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -7,18 +7,9 @@ const Carousel = () => {
   return (
     <div className="py-8">
       {/* Contenedor Grid que maneja filas y columnas */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols- gap-4">
         {/* Fila 1 */}
         <div className="card bg-white p-4 rounded-xl  hover:scale-105 transition transform duration-300">
-          <TravelPackageCard image={img} />
-        </div>
-        <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
-          <TravelPackageCard image={img} />
-        </div>
-        <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
-          <TravelPackageCard image={img} />
-        </div>
-        <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
           <TravelPackageCard image={img} />
         </div>
         <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
@@ -32,9 +23,24 @@ const Carousel = () => {
         <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
           <TravelPackageCard image={img} />
         </div>
+
+        {/* Fila 3 */}
         <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
           <TravelPackageCard image={img} />
         </div>
+        <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
+          <TravelPackageCard image={img} />
+        </div>
+
+        {/* Fila 4 */}
+        <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
+          <TravelPackageCard image={img} />
+        </div>
+        <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
+          <TravelPackageCard image={img} />
+        </div>
+
+        {/* Fila 5 */}
         <div className="card bg-white p-4 rounded-xl hover:scale-105 transition transform duration-300">
           <TravelPackageCard image={img} />
         </div>
