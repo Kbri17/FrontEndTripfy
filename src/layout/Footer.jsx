5635e055d042dbe9e2d3cf119ba84a90f8939b82
MoshMR <m.martinez.da@gmail.com>
2025-02-21 14:15:13 -0500

    Updated Footer component, add social media icons

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,13 +1,57 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faLinkedin,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="w-full h-16 bg-blue-500 flex items-center justify-start px-4">
-      <img src={logo} alt="Tripfy Logo" className="h-6" />
-      <span className="text-white text-l ml-[10px]">
-        &copy; {new Date().getFullYear()} Tripfy. Todos los derechos reservados.
-      </span>
+    <footer className="w-full h-16 bg-blue-500 flex items-center justify-between px-4">
+      <div className="flex items-center">
+        <img src={logo} alt="Tripfy Logo" className="h-6" />
+        <span className="text-white text-l ml-[10px]">
+          &copy; {new Date().getFullYear()} Tripfy. Todos los derechos
+          reservados.
+        </span>
+      </div>
+      <div className="flex space-x-4">
+        <a
+          href="https://www.facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="lg" />
+        </a>
+        <a
+          href="https://www.linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+        </a>
+        <a
+          href="https://www.twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white"
+        >
+          <FontAwesomeIcon icon={faTwitter} size="lg" />
+        </a>
+        <a
+          href="https://www.instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="lg" />
+        </a>
+      </div>
     </footer>
   );
 };
