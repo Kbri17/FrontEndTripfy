bf39bdb7eb6720a37bb7bdfc3ac654a294843d29
MoshMR <m.martinez.da@gmail.com>
2025-03-04 02:09:55 -0500

    seccion Categorias con iconos

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -1,49 +1,24 @@
-import React from "react";
-import img from "../src/assets/img.png";
-import img1 from "../src/assets/img1.jpg.jpg";
-import img2 from "../src/assets/img3.jpg.jpg";
+import { FaSun, FaSuitcase } from "react-icons/fa";
 
 const HeroSection = () => {
   return (
-    <section className="relative w-full h-[700px]">
-      {/* Imagen de fondo */}
-      <img
-        src={img} // Reemplaza con la URL o path de tu imagen
-        alt="Hero Background"
-        className="absolute inset-0 w-full h-full object-cover"
-      />
-      {/* Capa oscura para mejorar el contraste del texto */}
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      {/* Contenido centrado */}
-      <div className="relative z-10 flex flex-col items-center justify-center h-full  w-full text-center px-4">
-        <h1 className="text-4xl md:text-4xl text-white font-bold mb-4">
-          Conoce nuestras categorias
-        </h1>
-        <div className="flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-20">
-        <div className="relative flex flex-col items-center">
-            <img
-              src={img1}
-              alt="Categoria 1"
-              className="h-96 w-72 object-cover rounded-lg"
-            />
-            <button className="absolute bottom-4 left-4 px-6 py-3 bg-white text-wblue-600 rounded-lg hover:bg-blue-700 transition">
-              Full Day
-            </button>
-          </div>
-          <div className="relative flex flex-col items-center">
-            <img
-              src={img2}
-              alt="Categoria 2"
-              className="h-96 w-72 object-cover rounded-lg"
-            />
-            <button className="absolute bottom-4 left-4 px-6 py-3 bg-white text-wblue-600 rounded-lg hover:bg-blue-700 transition">
-              Paquetes
-            </button>
-          </div>
+    <nav className="flex items-center justify-center p-4 bg-gray-800 text-white w-full z-50 relative">
+      <div className="flex items-center space-x-16">
+        <div className="text-lg font-bold">Categorias</div>
+        <div className="flex space-x-8">
+          <button className="flex flex-col items-center p-2 bg-blue-500 text-white rounded-lg transition duration-300 hover:bg-white hover:text-blue-500">
+            <FaSun size={24} />
+            <span className="text-sm">Full Day</span>
+          </button>
+          <button className="flex flex-col items-center p-2 bg-green-500 text-white rounded-lg transition duration-300 hover:bg-white hover:text-green-500">
+            <FaSuitcase size={24} />
+            <span className="text-sm">Paquetes</span>
+          </button>
         </div>
       </div>
-    </section>
+    </nav>
   );
+
 };
 
-export default HeroSection;
+export default HeroSection;
