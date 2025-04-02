56bdbf88dfa7511938ed4e30d7c390df1d90fe73
Lorena Trujillo <lorenatrujillo991@gmail.com>
2025-04-01 23:33:33 -0500

    Galeria funcionando y agrego fecha a producto

--- src/pages/Reservas.jsx
+++ src/pages/Reservas.jsx
@@ -128,7 +128,7 @@ const Reservas = () => {
                   />
                 ) : (
                   <button
-                    className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition-colors duration-300 text-sm"
+                    className="bg-red-700 text-white px-4 py-2 rounded-lg hover:bg-red-900 border-2 border-white shadow-lg transition-colors duration-300 text-sm"
                     onClick={() => cancelarReserva(reserva.idReserva)}
                   >
                     ❌ Cancelar Reserva
